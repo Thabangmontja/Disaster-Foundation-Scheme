d2a29dec23b025b2fb8438424b0ccdf986f45152
Thabang Montja <thabangmontja20@gmail.com>
2023-10-19 08:58:43 +0000

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="APPR_POE.MvcApplication" Language="C#" %>
---
+++ User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace APPR_POE
+{
+    public enum User
+    {
+        NotAnonymous = 0,
+        Anonymous = 1,
+    }
+}
